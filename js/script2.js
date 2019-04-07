8ef9be006f3dabf1108efe02312657a90053fb82
Raghav Agrawal <47061939+code-Raghav@users.noreply.github.com>
2019-04-07 11:57:00 +0530

    Update script2.js

--- js/script2.js
+++ js/script2.js
@@ -1,49 +1,86 @@
 function displaycrops(){
-  //cropData();
   document.getElementById('crops').style.display = "block";
+
+
+  var cropno = document.getElementById('country').options.selectedIndex;
+  var crop;
+
+  if(cropno == 0)
+  {
+    crop = "wheat";
+
+    document.getElementById('p1').innerHTML = "13%";
+    document.getElementById('p2').innerHTML = "57%";
+    document.getElementById('p3').innerHTML = "12%";
+    document.getElementById('p4').innerHTML = "22%";
+
+
+  }
+  else if (cropno == 1) {
+    crop = "rice"
+    document.getElementById('p1').innerHTML = "63%%";
+    document.getElementById('p2').innerHTML = "24%";
+    document.getElementById('p3').innerHTML = "31%";
+    document.getElementById('p4').innerHTML = "17%";
+  }
+  else if (cropno == 2) {
+    crop = "maize"
+    document.getElementById('p1').innerHTML = "21%";
+    document.getElementById('p2').innerHTML = "16%";
+    document.getElementById('p3').innerHTML = "33%";
+    document.getElementById('p4').innerHTML = "16%";
+  }
+  else if (cropno == 3) {
+    crop = "pulses"
+    document.getElementById('p1').innerHTML = "3%";
+    document.getElementById('p2').innerHTML = "3%";
+    document.getElementById('p3').innerHTML = "24%";
+    document.getElementById('p4').innerHTML = "45%";
+  }
+
+document.getElementById('states').style.display = "none";
 }
+
 function displaystates(){
-  //locData();
   document.getElementById('states').style.display = "block";
-}
 
 
-$(".btnCrop").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".crops").offset().top},
-        'slow');
-});
+    var stateno = document.getElementById('country1').options.selectedIndex;
+    console.log(stateno);
 
-$(".btnState").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".states").offset().top},
-        'slow');
-});
+    if(stateno == 0)
+    {
 
+      document.getElementById('pp1').innerHTML = "63%";
+      document.getElementById('pp2').innerHTML = "24%";
+      document.getElementById('pp3').innerHTML = "31%";
+      document.getElementById('pp4').innerHTML = "17%";
 
 
-//data
-//var data = [[63, 13, 21, 3][24, 57, 16, 3][31, 12,33, 24][17, 22, 16, 45]];
-//var location = ["West Bengal", "Uttar Pradesh", "Karnataka", "Madhya Pradesh"];
-//var cropIndex = ["per0", "per1", "per2", "per4"];
-//var locationIndex = ["percent0", "percent1", "percent2", "percent3"];
+    }
+    else if (stateno == 1) {
 
-//function cropData(){
-//  var ind = document.getElementById('crop').value;
-//  var test = parseInt(ind);
-//  var count = 0;
-//  var crop = locationIndex[count].ind;
 
-//  for each (dataSet in data){
-//    var indNow = cropIndex[count];
-//    document.getElementById(indNow).innerHTML = data[count][crop];
-//    count += 1;
-//  }
-//}
 
-//function locData(){
-//  var loc = document.getElementById('country').value;
-//  var test2 = parseInt(loc);
-//  for each (percent in data[test2])
-//  document.getElementById(locNow).innerHTML = percent;
-//}
+      document.getElementById('pp1').innerHTML = "13%";
+      document.getElementById('pp2').innerHTML = "57%";
+      document.getElementById('pp3').innerHTML = "12%";
+      document.getElementById('pp4').innerHTML = "22%";
+    }
+    else if (stateno == 2) {
+
+      document.getElementById('pp1').innerHTML = "21%";
+      document.getElementById('pp2').innerHTML = "16%";
+      document.getElementById('pp3').innerHTML = "33%";
+      document.getElementById('pp4').innerHTML = "16%";
+    }
+    else if (stateno == 3) {
+
+      document.getElementById('pp1').innerHTML = "3%";
+      document.getElementById('pp2').innerHTML = "3%";
+      document.getElementById('pp3').innerHTML = "24%";
+      document.getElementById('pp4').innerHTML = "45%";
+    }
+
+    document.getElementById('country').style.display == "none"
+  }
