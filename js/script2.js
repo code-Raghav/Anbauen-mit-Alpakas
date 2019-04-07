ac5cc455af42bcc39d51eafe8ce53eacecaad0a0
Raghav Agrawal <araghav2003@gmail.com>
2019-04-07 10:47:05 +0530

    update

--- js/script2.js
+++ js/script2.js
@@ -1,19 +1,25 @@
 function displaycrops(){
-document.getElementById('crops').style.display = "block";
+  document.getElementById('crops').style.display = "block";
+
+  var cropno = document.getElementById('country').options.selectedIndex;
+  var crop;
+  if(cropno == 0){
+    crop = "wheat";
+  }
+  else if (cropno == 1) {
+    crop = "rice"
+  }
+  else if (cropno == 2) {
+    crop = "maize"
+  }
+  else if (cropno == 3) {
+    crop = "pulses"
+  }
+  console.log(crop);
+  console.log(cropno);
+  
+
 }
 function displaystates(){
 document.getElementById('states').style.display = "block";
 }
-
-
-$(".btnCrop").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".crops").offset().top},
-        'slow');
-});
-
-$(".btnState").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".states").offset().top},
-        'slow');
-});
