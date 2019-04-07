48ac82e22ef369893c009e518ef6a36065c91c48
elisabethschiele <elisabethschiele@gmail.com>
2019-04-07 06:22:39 +0200

    commented out last nights code

--- js/script2.js
+++ js/script2.js
@@ -19,10 +19,32 @@ $(".btnState").click(function() {
 });
 
 
-//data
-var data = [[63, 13, 21, 3][24, 57, 16, 3][31, 12,33, 24][17, 22, 16, 45]];
-var location = ["West Bengal", "Uttar Pradesh", "Karnataka", "Madhya Pradesh"];
+//function displayresult (){
+//  if {
+//    document.getelementById()
+//  }
+//}
 
-function cropData(stateId){
-  document.getElementById()
-}
+//data
+//var data = [[63, 13, 21, 3][24, 57, 16, 3][31, 12,33, 24][17, 22, 16, 45]];
+//var location = ["West Bengal", "Uttar Pradesh", "Karnataka", "Madhya Pradesh"];
+//var cropIndex = ["per0", "per1", "per2", "per4"];
+//var locationIndex = ["percent0", "percent1", "percent2", "percent3"];
+
+//function cropData(){
+//  var ind = document.getElementById('crop').value;
+//  var count = 0;
+//  var crop = locIndex[count];
+
+//  for each (dataSet in data){
+//    var indNow = cropIndex[count];
+//    document.getElementById(indNow).innerHTML = dataSet[ind];
+//    count += 1;
+//  }
+//}
+
+//function locData(){
+//  var loc = document.getElementById('country').value;
+//  for each (percent in data[loc])
+//  document.getElementById(locNow).innerHTML = percent;
+//}
